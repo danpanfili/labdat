5eb3e0cc217fc388da41740c38036176e34e5955
danpanfili <daniel.paul.panfili@gmail.com>
2024-04-01 18:15:36 -0400

    Delete labdat directory

--- labdat/experiment/__init__.py
+++
@@ -1,5 +0,0 @@
-import os, importlib
-
-subpackage_path             = os.path.dirname(__file__)
-submodules                  = [f[:-3] for f in os.listdir(subpackage_path) if f.endswith('.py') and f != '__init__.py']
-for module in submodules:   importlib.import_module(f".{module}", package=__name__)
